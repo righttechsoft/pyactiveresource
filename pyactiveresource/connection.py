e22df6bbe2c922244b39e1db3b0855c36435b2a4
neo <kirillyar>
2023-05-04 14:29:23 +0500

    remove sfy token from logs

--- pyactiveresource/connection.py
+++ pyactiveresource/connection.py
@@ -264,8 +264,6 @@ class Connection(object):
             # Insert basic authentication header
             request.add_header('Authorization', 'Basic ' + self.auth)
         if request.headers:
-            request.headers
-            request.headers.pop('X-shopify-access-token')
             header_string = '\n'.join([':'.join((k, v)) for k, v in filter(lambda h: h[0] != "X-shopify-access-token",
                                                                            six.iteritems(request.headers))])
             self.log.debug('request-headers:%s', header_string)
